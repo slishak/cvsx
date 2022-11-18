5ec7def823cdb894a76560fa3e954481669bd81c
Sam Lishak <sam@lishak.com>
2022-11-18 13:00:45 +0000

    Update simulation code to compare constant heart rate vs equivalent in dynamic HR model

--- examples/simulate.py
+++ examples/simulate.py
@@ -1,4 +1,5 @@
 from time import perf_counter
+from functools import partial
 
 import jax
 import jax.numpy as jnp
@@ -12,15 +13,21 @@ from cvsx import cardiac_drivers as drv
 from cvsx.unit_conversions import convert
 
 
-@jax.jit
-def main():
+@partial(jax.jit, static_argnums=0)
+def main(dynamic=False):
 
     rtol = 1e-3
     atol = 1e-6
 
+    # f_hr = lambda t: 80 + 20 * jnp.tanh(0.3 * (t - 20))
+    if dynamic:
+        f_hr = lambda t: jnp.full_like(t, fill_value=60.0)
+    else:
+        f_hr = 60.0
+
     cvs = models.SmoothInertialSmithCVS(
         parameter_source="revie",
-        cd=drv.GaussianCardiacDriver(),
+        cd=drv.SimpleCardiacDriver(hr=f_hr),
     )
 
     init_states = {
@@ -35,6 +42,9 @@ def main():
         "q_tc": convert(190.0661, "ml/s"),
         "q_pv": convert(0.0, "ml/s"),
     }
+    if dynamic:
+        init_states["s"] = 0.0
+
     nl_solver = diffrax.NewtonNonlinearSolver(
         rtol=rtol,
         atol=atol,
@@ -53,7 +63,7 @@ def main():
         term,
         ode_solver,
         0.0,
-        10.0,
+        40.0,
         None,
         init_states,
         args=(nl_solver,),
@@ -71,26 +81,51 @@ if __name__ == "__main__":
     with jax.default_device(jax.devices("cpu")[0]):
         print("Compile")
         t0 = perf_counter()
-        main()
+        main(dynamic=False)
+        main(dynamic=True)
         t1 = perf_counter()
         print(f"Compiled in {t1-t0:6f}s. Start timing")
-        res, out = main()
-        t2 = perf_counter()
-        print(f'Final time: {t2-t1:.6f}s, {res.stats["num_steps"]} steps')
 
-    fig, ax = plt.subplots(len(res.ys), 2, sharex=True)
+        for i in range(4):
+            ta = perf_counter()
+            res1, out1 = main(dynamic=False)
+            tb = perf_counter()
+            print(f'{tb-ta:.6f}s, {res1.stats["num_steps"]} steps')
+
+        for i in range(4):
+            ta = perf_counter()
+            res2, out2 = main(dynamic=True)
+            tb = perf_counter()
+            print(f'Dynamic HR: {tb-ta:.6f}s, {res2.stats["num_steps"]} steps')
+
+    all_states = res1.ys.keys() | res2.ys.keys()
+    fig, ax = plt.subplots(len(all_states), 2, sharex=True)
 
     min_q = 0.0
 
-    for i, (key, val) in enumerate(res.ys.items()):
-        ax[i, 0].plot(res.ts, val, ".-", label=key)
-        ax[i, 1].plot(res.ts, out[f"d{key}_dt"], ".-", label=key)
+    for i, key in enumerate(all_states):
+        try:
+            ax[i, 0].plot(res1.ts, res1.ys[key], ".-", label=key, markersize=1)
+        except KeyError:
+            pass
+        else:
+            ax[i, 1].plot(res1.ts, out1[f"d{key}_dt"], ".-", label=key, markersize=1)
+        try:
+            ax[i, 0].plot(res2.ts, res2.ys[key], ".-", label=key, markersize=1)
+        except KeyError:
+            pass
+        else:
+            ax[i, 1].plot(res2.ts, out2[f"d{key}_dt"], ".-", label=key, markersize=1)
         ax[i, 0].set_ylabel(key)
         ax[i, 1].set_ylabel(f"d{key}_dt")
 
     plt.figure()
-    plt.hist(jnp.log(jnp.diff(res.ts[~jnp.isinf(res.ts)])))
-    plt.xlabel("Log timestep")
-    plt.ylabel("Count")
+    plt.plot(res1.ts, out1["e_t"], label="Static")
+    plt.plot(res2.ts, out2["e_t"], label="Dynamic")
+
+    # plt.figure()
+    # plt.hist(jnp.log(jnp.diff(res.ts[~jnp.isinf(res.ts)])))
+    # plt.xlabel("Log timestep")
+    # plt.ylabel("Count")
 
     plt.show()
